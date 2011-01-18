e53113e312550433ae0cf7dc3fe1568884b964fb
Luca Barbato <lu_zero@gentoo.org>
2011-01-18 23:36:35 +0100

    Split swscale_template.c
    The swscale_template is split among ppc and x86, lots of code is
    duplicated unnecessary as code, before it was just duplicated as object

--- libswscale/swscale.c
+++ libswscale/swscale.c
@@ -1207,7 +1207,7 @@ static inline void monoblack2Y(uint8_t *dst, const uint8_t *src, long width, uin
 #undef COMPILE_TEMPLATE_ALTIVEC
 #define COMPILE_TEMPLATE_ALTIVEC 1
 #define RENAME(a) a ## _altivec
-#include "swscale_template.c"
+#include "ppc/swscale_template.c"
 #endif
 
 #if ARCH_X86
@@ -1222,7 +1222,7 @@ static inline void monoblack2Y(uint8_t *dst, const uint8_t *src, long width, uin
 #define COMPILE_TEMPLATE_MMX2 0
 #define COMPILE_TEMPLATE_AMD3DNOW 0
 #define RENAME(a) a ## _MMX
-#include "swscale_template.c"
+#include "x86/swscale_template.c"
 #endif
 
 //MMX2 versions
@@ -1235,7 +1235,7 @@ static inline void monoblack2Y(uint8_t *dst, const uint8_t *src, long width, uin
 #define COMPILE_TEMPLATE_MMX2 1
 #define COMPILE_TEMPLATE_AMD3DNOW 0
 #define RENAME(a) a ## _MMX2
-#include "swscale_template.c"
+#include "x86/swscale_template.c"
 #endif
 
 //3DNOW versions
@@ -1248,7 +1248,7 @@ static inline void monoblack2Y(uint8_t *dst, const uint8_t *src, long width, uin
 #define COMPILE_TEMPLATE_MMX2 0
 #define COMPILE_TEMPLATE_AMD3DNOW 1
 #define RENAME(a) a ## _3DNow
-#include "swscale_template.c"
+#include "x86/swscale_template.c"
 #endif
 
 #endif //ARCH_X86
